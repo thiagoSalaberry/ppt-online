87d5b27eb5ce0a680b98c343f9cd58b3c2a0e9c3
Thiago <thiagosalaberry99@gmail.com>
2024-02-24 23:14:12 -0300

    Source Code pro

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -6,17 +6,17 @@ export function Header() {
         <header className={styles["home-header"]}>
         <div className={styles["header-title"]}>
           <Move className={"izq"} move="piedra" size="small"></Move>
-          <TextComp tag="p" size="63px" weight="900">PIEDRA</TextComp>
+          <TextComp tag="p" size="54px" weight="900">PIEDRA</TextComp>
           <Move className={"der"} move="piedra" size="small"></Move>
         </div>
         <div className={styles["header-title"]}>
           <Move className={"izq"} move="papel" size="small"></Move>
-          <TextComp tag="p" size="63px" weight="900">PAPEL</TextComp>
+          <TextComp tag="p" size="54px" weight="900">PAPEL</TextComp>
           <Move className={"der"} move="papel" size="small"></Move>
         </div>
         <div className={styles["header-title"]}>
           <Move className={"izq"} move="tijera" size="small"></Move>
-          <TextComp tag="p" size="63px" weight="900">TIJERA</TextComp>
+          <TextComp tag="p" size="54px" weight="900">TIJERA</TextComp>
           <Move className={"der"} move="tijera" size="small"></Move>
         </div>
       </header>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,9 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Teko:wght@300..700&display=swap" rel="stylesheet"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@0,200..900;1,200..900&display=swap" rel="stylesheet"/>
       </Head>
       <body>
         <Main />
