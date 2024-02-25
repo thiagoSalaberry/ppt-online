db3a747e694bf3e9788247c51e5f71eae9666c65
Thiago <thiagosalaberry99@gmail.com>
2024-02-25 00:13:40 -0300

    button delay

--- ui/buttons/index.tsx
+++ ui/buttons/index.tsx
@@ -3,7 +3,7 @@ export function Button(props:ButtonProps) {
     const handleClick = () => {
         setTimeout(()=>{
             props.onClick && props.onClick();
-        }, 100)
+        }, 600)
         
     }
     return <button className={`${styles["button"]} ${styles[props.color ? props.color : ""]}`} onClick={handleClick}>{props.children}</button>
