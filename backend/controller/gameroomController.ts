e7c11f1c87e3d35e71f4c24550011db8737f8c99
Thiago <thiagosalaberry99@gmail.com>
2024-02-25 12:35:08 -0300

    gameroom fix

--- backend/controller/gameroomController.ts
+++ backend/controller/gameroomController.ts
@@ -5,13 +5,12 @@ import { v4 as uuidv4 } from "uuid";
 export async function createGameroom(player: PlayerData) {
   const longId = uuidv4(),
     shortId = Math.ceil(Math.random() * 1000000),
-    host = await getPlayer(player.name, player.pin);
-  if (host.error) return { error: host.error };
-  const history = {
-    hostWins: 0,
-    guestWins: 0,
-    draws: 0,
-  };
+    host = await getPlayer(player.name, player.pin),
+    history = {
+      hostWins: 0,
+      guestWins: 0,
+      draws: 0,
+    };
   const newGameroom = await Gameroom.createNewRoom({
     gameroomId: longId,
     shortRoomId: shortId,
@@ -39,13 +38,6 @@ export async function getGameroom(
         },
       };
     const guest = await getPlayer(name, Number(pin));
-    if (guest.error)
-      return {
-        error: {
-          message: "El usuario no existe.",
-          origin: "gameroomControllers.ts => getGameroom()",
-        },
-      };
     const addGuest = await gameroom.addGuest(
       guest.playerData.name,
       guest.playerId
