0d9398587d073473a6c5386633d6ec0d4eb6fcd3
Thiago <thiagosalaberry99@gmail.com>
2024-04-12 14:44:37 -0300

    sin sets

--- pages/api/setReady/[shortRoomId].ts
+++ pages/api/setReady/[shortRoomId].ts
@@ -1,13 +1,13 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
-import { setReady } from "@/backend/controller/gameroomController";
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const { shortRoomId } = req.query;
-  const { playerId } = req.body;
-  const ready = await setReady(String(shortRoomId), playerId);
-  if (!ready) return res.status(400).json("Algo salió mal");
-  if (ready) return res.status(200).json(ready);
-}
+// // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// import type { NextApiRequest, NextApiResponse } from "next";
+// import { setReady } from "@/backend/controller/gameroomController";
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   const { shortRoomId } = req.query;
+//   const { playerId } = req.body;
+//   const ready = await setReady(String(shortRoomId), playerId);
+//   if (!ready) return res.status(400).json("Algo salió mal");
+//   if (ready) return res.status(200).json(ready);
+// }
