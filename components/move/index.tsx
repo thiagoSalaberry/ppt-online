9f40ce576962a4574e40ba1bbebb773ed5098ea4
Thiago <thiagosalaberry99@gmail.com>
2024-04-15 15:44:53 -0300

    dejamos solamente la carpeta pages/api

--- components/move/index.tsx
+++
@@ -1,7 +0,0 @@
-import styles from "./move.module.css";
-export function Move(props:MoveProps) {
-    const handleSelect = (move:"piedra" | "papel" | "tijera") => {
-        if(props.onClick) props.onClick(move);
-    };
-    return <img src={`/${props.move}-linea-lleno.png`} alt="move.png" className={!props.selected ? `${styles["move"]} ${styles[props.size]} ${styles[props.className ? props.className : ""]}` : props.selected && props.selected == props.move ? `${styles["selected"]} ${styles[props.size]} ${styles[props.className ? props.className : ""]}` : `${styles["not-selected"]} ${styles[props.size]} ${styles[props.className ? props.className : ""]}`} onClick={!props.selected ? ()=>handleSelect(props.move!) : ()=>{}}/>
-}
--- ui/texts/index.tsx
+++
@@ -1,5 +0,0 @@
-import styles from "./texts.module.css";
-
-export function TextComp(props:TextProps) {
-    return (<props.tag style={{fontWeight: props.weight, fontSize: props.size || "24px", color: props.color, lineHeight: props.height, textAlign: props.align, margin: 0}}>{props.children}</props.tag>)
-}
