ed895ce1b5f474ad3c8692cbe781f779f0d6f842
Thiago <thiagosalaberry99@gmail.com>
2024-04-15 15:15:34 -0300

    API index

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,8 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+const API_BASE_URL = process.env.API_BASE_URL;
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  res.send("Esta es la API del proyecto PPT Online");
+  res.json({
+    message: "Esta es la API del piedra papel o tijeras online.",
+    branch: "DEV",
+    APIUrl: API_BASE_URL,
+  });
 }
